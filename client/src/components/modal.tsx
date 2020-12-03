27f6c62a290ff1fd5057c75fb5794e2b593a8641
Dmitriy Bakhanenko <bahanenko@gmail.com>
2020-12-03 13:50:02 +0200

    made modal reusable component

--- client/src/components/modal.tsx
+++ client/src/components/modal.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
-const Modal = (props: any) => {
+const Modal = ({
+  title,
+  body,
+  submit,
+  cancel,
+  negative,
+  primary,
+  onDismiss,
+  onSubmit
+}: any) => {
   const portal = document.querySelector('#modal');
+
+  const btnClass = () => {
+    if (negative) return 'ui button negative';
+    else if (primary) return 'ui button primary';
+    else return 'ui button';
+  };
 
   return portal
     ? ReactDOM.createPortal(
-        <div
-          onClick={() => history.push('/')}
-          className='ui dimmer modals visible active'
-        >
+        <div onClick={onDismiss} className='ui dimmer modals visible active'>
           <div
             onClick={e => e.stopPropagation()}
             className='ui standart modal visible active'
           >
-            <div className='header'>Delete Stream</div>
-            <div className='content'>
-              Are you sure that you want to delete the stream?
-            </div>
+            <div className='header'>{title}</div>
+            <div className='content'>{body}</div>
             <div className='actions'>
-              <button className='ui button red'>Delete</button>
-              <button className='ui button'>Cancel</button>
+              <button onClick={onSubmit} className={btnClass()}>
+                {submit}
+              </button>
+              <button className='ui button'>{cancel}</button>
             </div>
           </div>
         </div>,
